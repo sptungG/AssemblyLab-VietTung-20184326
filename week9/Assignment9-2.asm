339cba9e2e6dbb1b32c69ce29b600655b1557619
sptungG <ngoviettung154@gmail.com>
2021-05-05 20:43:06 +0700

    update assign9-2

--- week9/Assignment9-2.asm
+++ week9/Assignment9-2.asm
@@ -46,21 +46,8 @@ PrintInt:
 	jal PrintNewLine
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4
-	jr $ra
-.text
-PrintString:
-        addi $sp, $sp, -4
-	sw $ra, 0($sp)
-	addi $v0, $zero, 4
-	syscall
-	# Saving the return register so we can jump the PrintNewLine subprogram
-	addi $sp, $sp, -4
-	sw $ra, 0($sp)
-	jal PrintNewLine
-	lw $ra, 0($sp)
-	addi $sp, $sp, 4
-	jr $ra
 	
+	jr $ra
 .text
 Exit:
 	li $v0, 10
