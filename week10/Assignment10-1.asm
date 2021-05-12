92906d0f8a78b6a401aac5c291c06b9023229acc
sptungG <ngoviettung154@gmail.com>
2021-05-12 12:04:34 +0700

    update assignment 1 2

--- week10/Assignment10-1.asm
+++ week10/Assignment10-1.asm
@@ -13,49 +13,50 @@
 # list of integers. Read this procedure carefully,
 # especially indexing method
 
-# Pseudo code
-# max_sum = 0
-# max_sum_pref = 0
-# running_sum = 0
-# for i in range (len(A)):
-#	running_sum += A[i]
-#	if running_sum > max_sum:
-#		max_sum = running_sum
-#		max_sum_pref += 1
-	
-.data
-A: .word -2,6,-1,3,-2
-
-.text
-main:	
-	la	$a0, A
-	li	$a1, 5
-	j	mspfx
+.data 
+	A: .word 	1, 2, 5, -4, 6, -3 
+.text 
+main:       
+	la 	$a0,A             
+	li 	$a1,6             
+	j     	mspfx             
 	nop
-continue:
+continue: 
+lock:       
+	j  lock              
+	nop 
 end_of_main:
-	li	$v0, 10
-	syscall
 
+#-------------------------------------------------------------- 
+#procedure sort (ascending selection sort using pointer) 
+#register usage in sort program #$a0 pointer to the first element in unsorted part 
+#$a1 pointer to the last element in unsorted part 
+#$t0 temporary place for value of last element 
+#$v0 pointer to max element in unsorted part 
+#$v1 value of max element in unsorted part 
+#-------------------------------------------------------------- 
 
-mspfx:
-	addi	$v0, $zero, 0	# Initialize length in $v0 to 0
-	addi	$v1, $zero, 0	# Initialize max sum in $v1 = 0
-	addi	$t0, $zero, 0	# Initialize index i in $t0 = 0
-	addi	$t1, $zero, 0	# Initialize running sum in $t1 to 0
-loop:
-	sll	$t2, $t0, 2	# put 4i in $t2
-	add	$t3, $t2, $a0	# put 4i + A in $t3
-	lw	$t4, 0($t3)
-	add 	$t1 $t4, $t1	# add A[i] to runnign sum in $t1
-	slt	$t5, $v1, $t1	# set $t5 to 1 if max sum < new sum
-	bnez	$t5, mdfy	# if max sum < running sum, modify results
-	j	test
-mdfy:
-	addi	$v0, $t0, 1	# new max_sum prefix has length i + 1
-	addi	$v1, $t1, 0	# new max sum = running sum in $t1
-test:	
-	addi	$t0, $t0, 1	# advance the index i
-	slt	$t5, $t0, $a1	# set $t5 = 1 if i < n
-	bnez	$t5, loop 	# if true continue looping
-done:	j continue
+mspfx:  
+	addi 	$v0,$zero,0 		#initialize length in $v0 to 0         
+	addi 	$v1,$zero,0 		#initialize max sum in $v1to 0         
+	addi 	$t0,$zero,0 		#initialize index i in $t0 to 0         
+	addi 	$t1,$zero,0 		#initialize running sum in $t1 to 0 
+loop: 
+	add  	$t2,$t0,$t0       	#put 2i in $t2       
+	add 	$t2,$t2,$t2      	#put 4i in $t2  
+	add 	$t3,$t2,$a0       	#put 4i+A (address of A[i]) in $t3  	#cong dia chi m?i l?n t?ng 4
+	lw 	$t4,0($t3)  		#load A[i] from mem(t3) into $t4  	#gan gia tri A[i] cho $t4
+	add 	$t1,$t1,$t4       	#add A[i] to running sum in $t1   	#cong tong dang chay
+	slt 	$t5,$v1,$t1       	#set $t5 to 1 if max sum < new sum  	#tong moi lon hon maxsum hien tai thi $t5 = 1
+	bne 	$t5,$zero,mdfy  	#if max sum is less, modify results  	#neu t5 = 0 thi chuyen sang mdfy ngc lai ch?y tiep
+	j 	test        		#done? 
+mdfy: 
+	addi 	$v0,$t0,1  		#new max-sum prefix has length i+1  	#do dai tong max tang 1
+	addi 	$v1,$t1,0  		#new max sum is the running sum 	#gan gai tri max moi cho v1
+test: 
+	addi 	$t0,$t0,1  		#advance the index i  			#tang bien i
+	slt 	$t5,$t0,$a1      	#set $t5 to 1 if i<n  			#neu i < n thi t5 = 1
+	bne 	$t5,$zero,loop 		#repeat if i<n 				#neu t5 = 1 tuc la chua het ptu thi lap lai 
+done: 
+	j     	continue
+mspfx_end:
--- week10/Assignment10-2.asm
+++ week10/Assignment10-2.asm
@@ -21,44 +21,57 @@
 #	A[max_ptr], A[last_ptr] = A[last_ptr], A[max_ptr]
 #	last_ptr -= 1
 
-.data
-A: 	.word 7, -2, 5, 1, 5, 6, 7, 3, 6, 8, 8, 59, 5
-Aend: 	.word
-
-.text
-main:	
-	la 	$a0, A		# $a0 = A[0]
-	la	$a1, Aend	# $a1 = Aend
-	addi	$a1, $a1, -4	# Aend -= 1
-	j	sort		# sort
-after_sort:
-	li	$v0, 10		# exit
-	syscall
-
-sort:	
-	beq	$a0, $a1, done	# single element list is sorted
-	j	max		# call the max procedure
-after_max:
-	lw	$t0, 0($a1)	# load last element into $t0
-	sw	$t0, 0($v0)	# copy last value to max location
-	sw	$v1, 0($a1)	# copy max element to last location 
-	addi	$a1, $a1, -4	# decrement last pointer
-	j	sort
-done:	j 	after_sort
-	
-	
-max:
-	addi	$v0, $a0, 0	# init max pointer to first element
-	lw	$v1, 0($v0)	# init max value to first value
-	addi	$t0, $a0, 0	# init next pointer to first
-loop:
-	beq	$t0, $a1, ret	# if next=last, return
-	addi	$t0, $t0, 4	# advance to next element
-	lw	$t1, 0($t0)	# load next element into $t1
-	slt	$t2, $t1, $v1	# if element < max?
-	bnez	$t2, loop	# if true, continue looping
-	addi	$v0, $t0, 0	# next element is new max ptr
-	addi	$v1, $t1, 0	# next value is new max value
-	j	loop		# continue looping
-ret:
-	j	after_max
+.data 
+	A: 	.word 7, -2, 5, 1, 5, 6, 7, 3, 6, 8, 8, 59, 5 
+	Aend:   .word   
+.text 
+main:       
+	la   	$a0,A           	#$a0 = Address(A[0]) 
+	la   	$a1,Aend        					#load dia chi phan tu cuoi cho a1 la A[n]
+	addi 	$a1,$a1,-4      	#$a1 = Address(A[n-1])        	#load a1 = A[n-1]
+	j    	sort            	#sort 
+after_sort: 
+	li   	$v0, 10         	#exit             
+	syscall 
+end_main: 
+#-------------------------------------------------------------- 
+#procedure sort (ascending selection sort using pointer) 
+#register usage in sort program 
+#$a0 pointer to the first element in unsorted part 
+#$a1 pointer to the last element in unsorted part 
+#$t0 temporary place for value of last element 
+#$v0 pointer to max element in unsorted part 
+#$v1 value of max element in unsorted part 
+#-------------------------------------------------------------- 
+sort:       
+	beq   	$a0,$a1,done 		#single element list is sorted             
+	j   	max        		#call the max procedure 
+after_max:  
+	lw   	$t0,0($a1) 		#load last element into $t0      	#load gia tri n vao t0       
+	sw   	$t0,0($v0) 		#copy last element to max location      #
+	sw   	$v1,0($a1) 		#copy max value to last element             
+	addi    $a1,$a1,-4      	#decrement pointer to last element             
+	j   	sort  			#repeat sort for smaller list 
+done:       
+	j	after_sort 
+#----------------------------------------------------------------------- 
+#Procedure max 
+#function: fax the value and address of max element in the list 
+#$a0 pointer to first element 
+#$a1 pointer to last element 
+#----------------------------------------------------------------------- 
+max:  
+	addi 	$v0,$a0,0         	#init max pointer to first element   	#gan v0 la phan tu dau tien
+	lw    	$v1,0($v0)        	#init max value to first value  	#khoi tao gia tri cho max 
+	addi 	$t0,$a0,0         	#init next pointer to first 		
+loop:  
+	beq 	$t0,$a1,ret       	#if next=last, return  			#neu t0 la ptu cuoi cung thi thoat
+	addi 	$t0,$t0,4         	#advance to next element  		#neu t0 chua phai pt cuoi thi nhay den pt tiep theo
+	lw 	$t1,0($t0)        	#load next element into $t1  		#load gia tri cua no vao t1
+	slt 	$t2,$t1,$v1       	#(next)<(max) ?  			#neu t1 < max hien tai thi t1 = 1
+	bne 	$t2,$zero,loop 		#if (next)<(max), repeat  		#neu t2 = 1 thi chay lai loop
+	addi 	$v0,$t0,0       	#next element is new max element  	#
+	addi 	$v1,$t1,0       	#next value is new max value   		#neu t2 = 0 thi cap nhat gia tri max moi
+	j loop            	 	#change completed; now repeat 
+ret:  
+	j     after_max 
